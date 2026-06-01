e89e58c73811b92480b490ef0c2c4267142e12e3
Perry Van Wagoner <vanwper@login03.rc.byu.edu>
2026-06-01 08:52:00 -0600

    updated process7888.sh to process 7888 data for alignment to references

--- process7888.sh
+++ process7888.sh
@@ -19,10 +19,30 @@ module load miniforge3
 #mamba activate samtools_env
 #samtools fastq $bam | bgzip -c > $workingdir/7888/7888.fastq.gz
 
-mamba activate xengsort
-xengsort classify \
-       	--index /home/vanwper/nobackup/autodelete/neomex/xeng_data/k25/neomex.hash \
-       	--fastq /home/vanwper/nobackup/autodelete/neomex/7888/7888.fastq.gz \
-	--prefix /home/vanwper/nobackup/autodelete/neomex/7888/7888 \
-	--mode count \
-	--compression gz
+#use xengsort to classify the reads of the sample by which parent it comes from host is arizonae and graft is marmoratus
+#mamba activate xengsort
+#xengsort classify \
+       	#--index /home/vanwper/nobackup/autodelete/neomex/xeng_data/k25/neomex.hash \
+       	#--fastq /home/vanwper/nobackup/autodelete/neomex/7888/7888.fastq.gz \
+	#--prefix /home/vanwper/nobackup/autodelete/neomex/7888/7888 \
+	#--mode count \
+	#--compression gz
+
+#Index for the reference files is already created so just need to align
+#the graft and host files to their respective index
+mamba activate pacbioProcessing
+pbmm2 align \
+	$workingdir/reference/BaumannLab/a_arizonae_AspAri2.0.mmi \
+	$workingdir/7888/7888-host.fq.gz \
+	$workingdir/7888/7888-host.bam \
+	--preset CCS \
+	--sort
+echo "host processing complete"
+
+pbmm2 align \
+	$workingdir/reference/BaumannLab/a_marmoratus_AspMarm2.0.mmi \
+	$workingdir/7888/7888-graft.fq.gz \
+	$workingdir/7888/7888-graft.bam \
+	--preset CCS \
+	--sort
+echo "processing complete"
