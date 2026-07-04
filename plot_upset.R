d31163da7a147c976249f55fb80f22f4d62942f0
Perry Van Wagoner <vanwper@login01.rc.byu.edu>
2026-07-04 14:08:33 -0600

    updated upset plot to look better

--- plot_upset.R
+++ plot_upset.R
@@ -10,15 +10,16 @@ df <- read.csv(input, check.names = FALSE)
 rownames(df) <- NULL
 
 sample_cols <- colnames(df)[-1]
+sample_cols <- c("7888", "7890", "7891", "7892", "7893", "7894", "7895", "7896", "7897")
 
-pdf(output, width=10, height=6)
+pdf(output, width=9, height=7)
 
 upset(
       df,
       intersect = sample_cols,
       name = "Individuals",
       width_ratio = 0.25,
-      n_intersections = 30,
+      n_intersections = 15,
       min_degree = 2,
       sort_intersections_by = "cardinality"
 )
